b1a717d4981a2ca8261e2c36292a423144ff00f7
Anastasiya Terletskaya <asuta1000@mail.ru>
2024-10-11 18:02:37 +0500

    fix condition if time

--- tests/homework.py
+++ tests/homework.py
@@ -9,7 +9,7 @@ def test_dark_theme_by_time():
     is_dark_theme = None
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    if 6 < current_time.hour <= 22:
+    if 6 <= current_time.hour < 22:
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -35,7 +35,7 @@ def test_dark_theme_by_time_and_user_choice():
     elif dark_theme_enabled_by_user == False:
         is_dark_theme = False
     else:
-        if 6 < current_time.hour <= 22:
+        if 6 <= current_time.hour < 22:
             is_dark_theme = False
         else:
             is_dark_theme = True
